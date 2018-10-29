858629835a5582498b61d99a11e0b0d9de147741
jcc2303 <cabrera.javier@gmail.com>
2018-10-29 03:30:58 +0100

    publish: another fix
    generated from commit 2b311331f4ad8e575adbb1929462ac254ed11c5a

--- TODO.md
+++
@@ -1,4 +0,0 @@
-
-# TODO:
-migrate react-addons-css-transition-group to v2
-warning " > react-addons-css-transition-group@15.6.2" has incorrect peer dependency "react@^15.4.2".
---
+++ precache-manifest.93c8d3c73375b878f9e2eb9784ae4867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "380daeb646d020130052",
+    "url": "/static/js/main.380daeb6.chunk.js"
+  },
+  {
+    "revision": "8f8e85eb1d10fddf8645",
+    "url": "/static/js/1.8f8e85eb.chunk.js"
+  },
+  {
+    "revision": "380daeb646d020130052",
+    "url": "/static/css/main.7e18b3e2.chunk.css"
+  },
+  {
+    "revision": "04308e0e5c260c2a7af41dd19a96ed08",
+    "url": "/index.html"
+  }
+];
