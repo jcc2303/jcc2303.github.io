ed59f7717c6b3c9e81545172370c610847ce1ea4
jcc2303 <cabrera.javier@gmail.com>
2018-10-29 04:11:09 +0100

    added CV in pdf plus button

--- src/components/section/about.js
+++ src/components/section/about.js
@@ -43,16 +43,14 @@ export default class About  extends React.Component {
                                     </a>
                                 </p>
                             </div>
-                            {/*
                             <div className='columns download'>
                                 <p>
-                                    <a href='#' className='button'>
+                                    <a href='resume.pdf' className='button'>
                                         <i className='fa fa-download'></i>
                                         Download Resume
                                     </a>
                                 </p>
                             </div>
-                            */}
                         </div>
                     </div>
                 </div>
