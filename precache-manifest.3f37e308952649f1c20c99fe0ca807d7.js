d95dcdaa58d3b1ec223a67356b5e90fa5d4ee4fa
Javier Cabrera <cabrera.javier@gmail.com>
2020-05-13 15:45:37 +0200

    Updates

---
+++ precache-manifest.3f37e308952649f1c20c99fe0ca807d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cd06b0b58b868d1b475cb2f6e809b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa1aa65871f64a64d53",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "983c291ae8132792eb21",
+    "url": "/static/js/2.1e87a6c0.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.1e87a6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa1aa65871f64a64d53",
+    "url": "/static/js/main.9b11554f.chunk.js"
+  },
+  {
+    "revision": "e4e85ba9b1bc36a7599c",
+    "url": "/static/js/runtime-main.b2f1f2b5.js"
+  }
+]);
